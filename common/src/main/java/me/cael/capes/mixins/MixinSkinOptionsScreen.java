1b74ab209fe42a96ea6b39e7cc2ec5f23ac11352
Cassian Godsted <cassian@godsted.com>
2024-06-13 14:14:43 -0400

    Capes for Fabric 1.21
    This is a port of Capes for 1.21. This currently only supports Fabric, as Architectury's NeoForge Yarn mappings aren't yet updated for 1.21

--- common/src/main/java/me/cael/capes/mixins/MixinSkinOptionsScreen.java
+++ common/src/main/java/me/cael/capes/mixins/MixinSkinOptionsScreen.java
@@ -4,8 +4,11 @@ import me.cael.capes.menu.SelectorMenu;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
 import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.client.option.GameOptions;
+import net.minecraft.entity.player.PlayerModelPart;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,11 +17,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(SkinOptionsScreen.class)
 public class MixinSkinOptionsScreen extends GameOptionsScreen {
 
     @Unique
-    private static final Identifier CAPE_OPTIONS_ICON_TEXTURE = new Identifier("capes","icon/cape_options");
+    private static final Identifier CAPE_OPTIONS_ICON_TEXTURE = Identifier.of("capes","icon/cape_options");
     @Unique
     private final SelectorMenu selectorMenu = new SelectorMenu(this, this.gameOptions);
 
@@ -26,8 +32,20 @@ public class MixinSkinOptionsScreen extends GameOptionsScreen {
         super(parent, gameOptions, title);
     }
 
-    @Inject(method = "init", at = @At("RETURN"))
-    protected void init(CallbackInfo info) {
+    @Override
+    public void addOptions() {
+        List<ClickableWidget> list = new ArrayList<>();
+        PlayerModelPart[] var2 = PlayerModelPart.values();
+
+        for (PlayerModelPart playerModelPart : var2) {
+            list.add(CyclingButtonWidget.onOffBuilder(this.gameOptions.isPlayerModelPartEnabled(playerModelPart)).build(playerModelPart.getOptionName(), (cyclingButtonWidget, boolean_) -> {
+                this.gameOptions.togglePlayerModelPart(playerModelPart, boolean_);
+            }));
+        }
+
+        list.add(this.gameOptions.getMainArm().createWidget(this.gameOptions));
+        this.body.addAll(list);
         this.addDrawableChild(TextIconButtonWidget.builder(Text.empty(), (buttonWidget) -> this.client.setScreen(selectorMenu), true).dimension(20, 20).texture(CAPE_OPTIONS_ICON_TEXTURE, 16, 16).build()).setPosition(this.width / 2 - 179, this.height / 6);
+
     }
 }
