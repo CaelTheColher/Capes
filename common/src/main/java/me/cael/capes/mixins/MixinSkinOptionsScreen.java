993664b8e08f07f9b9fabda32567ef0eabc82e5c
CaelTheColher <16297430+CaelTheColher@users.noreply.github.com>
2024-06-15 10:59:14 -0300

    1.21 moment (no kff yet and mojang broke something with screens and idk what to do about either of those)

--- common/src/main/java/me/cael/capes/mixins/MixinSkinOptionsScreen.java
+++ common/src/main/java/me/cael/capes/mixins/MixinSkinOptionsScreen.java
@@ -4,11 +4,8 @@ import me.cael.capes.menu.SelectorMenu;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
-import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.gui.widget.CyclingButtonWidget;
-import net.minecraft.client.gui.widget.TextIconButtonWidget;
+import net.minecraft.client.gui.widget.*;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.entity.player.PlayerModelPart;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,35 +14,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Mixin(SkinOptionsScreen.class)
-public class MixinSkinOptionsScreen extends GameOptionsScreen {
+public abstract class MixinSkinOptionsScreen extends GameOptionsScreen {
 
     @Unique
     private static final Identifier CAPE_OPTIONS_ICON_TEXTURE = Identifier.of("capes","icon/cape_options");
     @Unique
-    private final SelectorMenu selectorMenu = new SelectorMenu(this, this.gameOptions);
+    private final SelectorMenu capes$selectorMenu = new SelectorMenu(this, this.gameOptions);
 
     public MixinSkinOptionsScreen(Screen parent, GameOptions gameOptions, Text title) {
         super(parent, gameOptions, title);
     }
 
-    @Override
-    public void addOptions() {
-        List<ClickableWidget> list = new ArrayList<>();
-        PlayerModelPart[] var2 = PlayerModelPart.values();
-
-        for (PlayerModelPart playerModelPart : var2) {
-            list.add(CyclingButtonWidget.onOffBuilder(this.gameOptions.isPlayerModelPartEnabled(playerModelPart)).build(playerModelPart.getOptionName(), (cyclingButtonWidget, boolean_) -> {
-                this.gameOptions.togglePlayerModelPart(playerModelPart, boolean_);
-            }));
-        }
-
-        list.add(this.gameOptions.getMainArm().createWidget(this.gameOptions));
-        this.body.addAll(list);
-        this.addDrawableChild(TextIconButtonWidget.builder(Text.empty(), (buttonWidget) -> this.client.setScreen(selectorMenu), true).dimension(20, 20).texture(CAPE_OPTIONS_ICON_TEXTURE, 16, 16).build()).setPosition(this.width / 2 - 179, this.height / 6);
-
+    @Inject(method = "addOptions", at = @At("RETURN"))
+    public void addOptions(CallbackInfo info) {
+        this.addDrawableChild(TextIconButtonWidget.builder(Text.empty(), (buttonWidget) -> this.client.setScreen(capes$selectorMenu), true).dimension(20, 20).texture(CAPE_OPTIONS_ICON_TEXTURE, 16, 16).build()).setPosition(body.getRowLeft() - 25, body.getY() + 4);
     }
 }
